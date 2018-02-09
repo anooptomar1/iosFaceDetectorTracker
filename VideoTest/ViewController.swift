c97f786d39b57fb1564ecc3343f09d1049283d22
Kim Tae-hoon <polphit@tae-hoons-macbook-pro.local>
2018-02-09 12:45:34 +0900

    Initial Commit

---
+++ VideoTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoTest
+//
+//  Created by Tae-hoon Kim on 09/02/2018.
+//  Copyright © 2018 Tae-hoon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
